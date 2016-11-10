2035d11aad2ef12eb54ad3d64fd57b452081da1d
e1r0nd <a.merezhanyi@gmail.com>
2016-11-10 13:37:52 +0100

    try promises

--- es6-babel-grunt/index.js
+++ es6-babel-grunt/index.js
@@ -4,3 +4,38 @@ let i = 10;
 let str = `test -${i}- message`;
 
 console.log(str);
+/*
+// Создаётся объект promise
+let promise = new Promise((resolve, reject) => {
+
+  setTimeout(() => {
+    // переведёт промис в состояние fulfilled с результатом "result"
+    resolve("result");
+  }, 1000);
+
+});
+
+// promise.then навешивает обработчики на успешный результат или ошибку
+promise
+  .then(
+    result => {
+      // первая функция-обработчик - запустится при вызове resolve
+      console.log("Fulfilled: " + result); // result - аргумент resolve
+    },
+    error => {
+      // вторая функция - запустится при вызове reject
+      console.log("Rejected: " + error); // error - аргумент reject
+    }
+  );
+*/
+let timeNow = new Date();
+let delay = ms => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  })
+};
+
+delay(1000)
+  .then(() => console.log("Hello!"));
