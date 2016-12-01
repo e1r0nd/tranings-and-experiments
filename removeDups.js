15d34f478773a1c0e29cdeee463a765a0d2ef7a6
e1r0nd <a.merezhanyi@gmail.com>
2016-12-02 00:02:10 +0100

    added removeDups (arrays)

--- removeDups.js
+++ removeDups.js
@@ -1,1 +1,30 @@
-var arr = [1, 2, 3, 3, 3, 4, 5, 5, 6];
+function removeDups(arr) {
+  let result = [];
+  
+  arr.map(function(itm) {
+    if (result.indexOf(itm) === -1) {
+      result.push(itm);
+    }
+  });
+  
+  return result;
+};
+
+function testIt(inputArray, resultArray) {
+  var isEqual = true;
+  
+  inputArray.forEach(function(itm) {
+    if (resultArray.indexOf(itm) === -1) {
+      return isEqual = false;
+    }
+  });
+  
+  return isEqual;
+};
+
+let inputArray = [1, 2, 3, 3, 3, 4, 5, 5, 6];
+let inputWrongArray = [0, 1, 2, 3, 3, 3, 4, 5, 5, 6];
+let resultArray = [1, 2, 3, 4, 5, 6];
+
+console.log('test should fail: ' + ((testIt(removeDups(inputWrongArray), resultArray)) ? 'passed' : 'failed'));
+console.log('test should pass: ' + ((testIt(removeDups(inputArray), resultArray)) ? 'passed' : 'failed'));
