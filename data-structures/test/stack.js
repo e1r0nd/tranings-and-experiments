fc52d018e185c1d9dbbacfa000ae614a6fcb3cef
e1r0nd <a.merezhanyi@gmail.com>
2016-12-04 00:59:28 +0100

    add Stack

---
+++ data-structures/test/stack.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var assert = require('assert');
+var Stack = require('../stack').Stack;
+
+describe('Stack', function() {
+  it('should use LIFO', () => {
+    let stack = new Stack();
+    stack.push(1);
+    stack.push(2);
+    assert.equal(stack._length, 2);
+    assert.equal(stack.peek(), 2);
+    assert.equal(stack.peek(), 2);
+    assert.equal(stack.pop(), 2);
+    assert.equal(stack.pop(), 1);
+    assert.equal(stack._length, 0);
+  });
+});
