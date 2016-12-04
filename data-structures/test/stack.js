4a23d8b5288790923d012b633376b15c7aeed060
e1r0nd <a.merezhanyi@gmail.com>
2016-12-04 01:03:06 +0100

    clean up

--- data-structures/test/stack.js
+++ data-structures/test/stack.js
@@ -6,8 +6,10 @@ var Stack = require('../stack').Stack;
 describe('Stack', function() {
   it('should use LIFO', () => {
     let stack = new Stack();
+    
     stack.push(1);
     stack.push(2);
+    
     assert.equal(stack._length, 2);
     assert.equal(stack.peek(), 2);
     assert.equal(stack.peek(), 2);
