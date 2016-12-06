0bbe43f4cc7f77894b8d714b88fb726324ab47c6
e1r0nd <a.merezhanyi@gmail.com>
2016-12-06 23:01:17 +0100

    add tests for LinkedList

---
+++ data-structures/linkedlist.js
@@ -0,0 +1,12 @@
+/*****************************************************
+* Class: Linked List. Data Structures in JS, Nov 2016
+* Anatol Marezhanyi http://linkedin.com/in/merezhany/ 
+*****************************************************/
+'use strict';
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this._length = 0;
+  }
+}
---
+++ data-structures/test/linkedlist.js
@@ -0,0 +1,25 @@
+/*****************************************************
+* Test: Linked List. Data Structures in JS, Nov 2016
+* Anatol Marezhanyi http://linkedin.com/in/merezhany/ 
+*****************************************************/
+
+'use strict';
+
+let assert = require('assert');
+let LinkedList = require('../linkedlist').LinkedList;
+
+describe('Linked List', function() {
+  it('should use List', () => {
+    let menu = new LinkedList();
+    
+    menu.add('home', 0);
+    menu.add('edit', 1);
+    menu.add('about', 2);
+    
+    assert.equal(menu._length, 3);
+    assert.equal(menu.get(2), 'about');
+    menu.remove(2)
+    assert.equal(menu.get(2), 'edit');
+    assert.equal(menu._length, 2);
+  });
+});
