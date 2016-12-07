1a340643372fb835c0821da408aa7572a2796af9
e1r0nd <a.merezhanyi@gmail.com>
2016-12-07 10:53:29 +0100

    added LinkedList

--- data-structures/linkedlist.js
+++ data-structures/linkedlist.js
@@ -6,7 +6,54 @@
 
 class LinkedList {
   constructor() {
-    this.head = null;
+    this._head = null;
     this._length = 0;
   }
+  
+  add(value, position) {
+    let node = {
+      _value: value,
+      _next: null
+    };
+
+    if (position === 0) {
+      node._next = this._head;
+      this._head = node;
+    } else {
+      let prev = this.get(position - 1);
+      let current = prev._next;
+      node._next = current;
+      prev._next = node;
+    }
+
+    this._length++;
+  }
+  
+  remove(position) {
+    if (position === 0) {
+      this._head = this._head._next;
+    } else {
+      let prev = this.get(position - 1);
+      prev._next = prev._next._next;
+    }
+
+    this._length--;
+  }
+  
+  get(position) {
+    if (position >= this._length) {
+      console.log('out of range');
+    }
+
+    let current = this._head;
+    for (let i = 0; i < position; i++) {
+      current = current._next;
+    }
+
+    return current;
+  }
 }
+
+module.exports = {
+  LinkedList: LinkedList
+};
--- data-structures/test/linkedlist.js
+++ data-structures/test/linkedlist.js
@@ -10,16 +10,17 @@ let LinkedList = require('../linkedlist').LinkedList;
 
 describe('Linked List', () => {
   it('should use List', () => {
-    let menu = new LinkedList();
+    let slideShow = new LinkedList();
     
-    menu.add('home', 0);
-    menu.add('edit', 1);
-    menu.add('about', 2);
-    
-    assert.equal(menu._length, 3);
-    assert.equal(menu.get(2), 'about');
-    menu.remove(2)
-    assert.equal(menu.get(2), 'edit');
-    assert.equal(menu._length, 2);
+    slideShow.add('image-001', 0);
+    slideShow.add('image-002', 1);
+    slideShow.add('image-003', 2);
+
+    assert.equal(slideShow._length, 3);
+    assert.equal(slideShow.get(1)._value, 'image-002');
+    slideShow.remove(1)
+
+    assert.equal(slideShow.get(1)._value, 'image-003');
+    assert.equal(slideShow._length, 2);
   });
 });
