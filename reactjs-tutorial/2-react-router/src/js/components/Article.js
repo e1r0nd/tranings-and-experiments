5441e5d25a2c4ffd530d031715023e6a5bd25889
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2017-05-25 16:10:07 +0200

    changed path

--- reactjs-tutorial/2-react-router/src/js/components/Article.js
+++ reactjs-tutorial/2-react-router/src/js/components/Article.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { IndexLink, Link } from "react-router";
 
 export default class Article extends React.Component {
+  selectArticle() {
+    // this.props.history.push(null, `/archieves/${this.props.title}`);
+    console.log(this);
+    return false;
+  }
+
   render() {
     const { title } = this.props;
 
     return (
-      <div class="col-md-4">
+      <div class="col-md-4 {selected}">
         <h4>{title}</h4>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
-        <a class="btn btn-default" href="#">More Info</a>
+        <a class="btn btn-default" href="#/archives/abc" onClick={this.selectArticle.bind(this)}>More Info</a>
       </div>
     );
   }
