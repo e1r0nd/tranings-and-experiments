be0ae58ddac3d8170e5aa6ccad6f39fda746439e
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-01-05 10:28:29 +0100

    solver JS (HR) Day0

---
+++ hackerRank/10-Days-of-Javascript/Day0-Hello-World.js
@@ -0,0 +1,19 @@
+/**
+ * Day 0: Hello, World!
+ * https://www.hackerrank.com/challenges/js10-hello-world
+ * Task
+ * A greeting function is provided for you in the editor below. It has one
+ * parameter, . Perform the following tasks to complete this challenge:
+ * Use console.log() to print Hello, World! on a new line in the console,
+ * which is also known as stdout or standard output. The code for this
+ * portion of the task is already provided in the editor.
+ * Use console.log() to print the contents of  (i.e., the argument passed to
+ * main).
+ */
+function greeting(parameterVariable) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    console.log(parameterVariable);
+}
