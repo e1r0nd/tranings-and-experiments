419ef33c4226f7722b7bb01aad0034432c9c3dca
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-03-03 00:51:15 +0100

    added Router

---
+++ React-For-Beginners/catch-of-the-day/src/components/NotFound.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+const NotFound = () => (
+    <header className="top">
+        <h1>Page not found</h1>
+    </header>
+);
+
+export default NotFound;
--- React-For-Beginners/catch-of-the-day/src/index.js
+++ React-For-Beginners/catch-of-the-day/src/index.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import StorePicker from "./components/StorePicker";
-import App from "./components/App";
+import Router from "./components/Router";
 import "./css/style.css";
 
 render(
-    <App />,
+    <Router />,
     document.querySelector("#main")
 );
