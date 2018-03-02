87aef02ea6e105a7af0f4dbdfc79dfc3dee9f647
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-03-02 23:09:17 +0100

    added Components

---
+++ React-For-Beginners/catch-of-the-day/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+const Header = ({tagline}) => (
+    <header className="top">
+        <h1>Catch
+            <span className="ofThe">
+                <span className="of">of</span>
+                <span className="the">the</span>
+            </span>
+            Day
+        </h1>
+        <h3 className="tag-line">
+            <span>{tagline}</span>
+        </h3>
+    </header>
+);
+
+export default Header;
--- React-For-Beginners/catch-of-the-day/src/index.js
+++ React-For-Beginners/catch-of-the-day/src/index.js
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
 import StorePicker from "./components/StorePicker";
+import App from "./components/App";
+import "./css/style.css";
 
 render(
-    <StorePicker />,
+    <App />,
     document.querySelector("#main")
 );
