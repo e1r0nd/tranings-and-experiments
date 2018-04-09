f1dce333772858fdf7f2c7d23f62f9c8e8a64425
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-09 20:50:28 +0200

    added 2 pages and router

--- firebase-node/functions/index.js
+++ firebase-node/functions/index.js
@@ -1,15 +1,45 @@
 const functions = require('firebase-functions');
+const firebase = require('firebase-admin');
 const express = require('express');
+const engines = require('consolidate');
+const resolve = require('url');
 
-const app = express();
+const firebaseApp = firebase.initializeApp(
+  functions.config().firebase
+);
 
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
+// function getFacts() {
+//   const ref = firebase.database().ref('facts');
+//   return ref.once('value').then(snap => snap.val());
+// }
+const getFacts = () => new Promise((resolve, reject) => {
+  const facts =
+    [
+      {"text":"fact1"},
+      {"text":"fact2"},
+      {"text":"fact3"}
+    ];
+
+  return resolve(facts);
 });
 
-app.get('/timestamp-cached', (request, response) => {
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+app.get('/', (request, response) => {
   response.set('Cached-Control', 'public, max-age=300, s-maxage=600');
-  response.send(`${Date.now()}`);
+  getFacts().then(facts => {
+    response.render('index', { facts });
+  });
+});
+
+app.get('/facts.json', (request, response) => {
+  response.set('Cached-Control', 'public, max-age=300, s-maxage=600');
+  getFacts().then(facts => {
+    response.json(facts);
+  });
 });
 
 exports.app = functions.https.onRequest(app);
