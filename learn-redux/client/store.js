012ad0b3085662f8c5ec466a2ac0c5a68f033ba2
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-18 12:57:09 +0200

    added redux actions

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -1,15 +1,15 @@
-import { createStore, compose } from 'redux';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { browserHistory } from 'react-router';
+import { createStore, compose } from "redux";
+import { syncHistoryWithStore } from "react-router-redux";
+import { browserHistory } from "react-router";
 
-import rootReducer from './reducers/index';
+import rootReducer from "./reducers/index";
 
-import comments from './data/comments';
-import posts from './data/posts';
+import comments from "./data/comments";
+import posts from "./data/posts";
 
 const defaultState = {
-    posts,
-    comments
+  posts,
+  comments
 };
 
 const store = createStore(rootReducer, defaultState);
