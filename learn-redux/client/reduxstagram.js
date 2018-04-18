3679b5a75fd27a39e1589a3dff4ecf3ba5077631
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-18 13:21:19 +0200

    added actions

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,21 +1,21 @@
 // let's go!
-import React from 'react';
-import { render } from 'react-dom';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import React from "react";
+import { render } from "react-dom";
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
 
-import css from './styles/style.styl';
+import css from "./styles/style.styl";
 
-import Main from './components/Main';
-import Single from './components/Single';
-import PhotoGrid from './components/PhotoGrid';
+import Main from "./components/Main";
+import Single from "./components/Single";
+import PhotoGrid from "./components/PhotoGrid";
 
 const router = (
-    <Router history={browserHistory}>
-        <Route path="/" component={Main}>
-            <IndexRoute component={PhotoGrid}></IndexRoute>
-            <Route path="/view/:postId" component={Single}></Route>
-        </Route>
-    </Router>
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid} />
+      <Route path="/view/:postId" component={Single} />
+    </Route>
+  </Router>
 );
 
-render(router, document.getElementById('root'));
+render(router, document.getElementById("root"));
