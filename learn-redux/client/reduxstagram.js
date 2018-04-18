dab7d7a8b7646f516bb18e185e5ee88b76e5a91b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-18 15:28:37 +0200

    added reducers

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -1,7 +1,8 @@
 // let's go!
 import React from "react";
 import { render } from "react-dom";
-import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { Router, Route, IndexRoute } from "react-router";
+import { Provider } from "react-redux";
 
 import css from "./styles/style.styl";
 
@@ -9,13 +10,17 @@ import Main from "./components/Main";
 import Single from "./components/Single";
 import PhotoGrid from "./components/PhotoGrid";
 
+import store, { history } from "./store";
+
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={Main}>
-      <IndexRoute component={PhotoGrid} />
-      <Route path="/view/:postId" component={Single} />
-    </Route>
-  </Router>
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={Main}>
+        <IndexRoute component={PhotoGrid} />
+        <Route path="/view/:postId" component={Single} />
+      </Route>
+    </Router>
+  </Provider>
 );
 
 render(router, document.getElementById("root"));
