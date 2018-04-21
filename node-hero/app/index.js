7a4c55a09c6767de60e284dbf54621411b6bd399
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-21 18:07:41 +0200

    added dependencies

--- node-hero/app/index.js
+++ node-hero/app/index.js
@@ -1,5 +1,8 @@
 const calc = require("./calc");
+const _ = require("lodash");
 const numbersToAdd = [3, 4, 10, 2];
 
 const result = calc.sum(numbersToAdd);
 console.log(`The result is: ${result}`);
+
+_.assign({ a: 1 }, { b: 2 }, { c: 3 });
