2184b54dc14bb18ef50f1becc6f2074ae3e19fcb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-25 23:58:06 +0200

    added a page

--- node-hero/index.js
+++ node-hero/index.js
@@ -5,6 +5,8 @@ const path = require("path");
 const express = require("express");
 const exphbs = require("express-handlebars");
 
+const rp = require("request-promise");
+
 const app = express();
 app.engine(
   ".hbs",
@@ -16,6 +18,26 @@ app.engine(
 );
 app.set("view engine", ".hbs");
 app.set("views", path.join(__dirname, "views"));
+
+app.get("/aw", (req, res) => {
+  rp({
+    uri:
+      "http://apidev.accuweather.com/currentconditions/v1/2-273125_1_AL.json",
+    qs: {
+      language: en,
+      apiKey: "hoArfRosT1215"
+    },
+    json: true
+  })
+    .then(data => {
+      console.log(data);
+      res.render("index", data);
+    })
+    .catch(err => {
+      console.log(err);
+      res.render("error");
+    });
+});
 
 app.get("/", (request, response) => {
   response.render("home", {
