a9699cf70d06d7a78112a632a9d148c631ff406b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-04-28 01:50:31 +0200

    init

---
+++ productivity-quotes/app/index.js
@@ -0,0 +1,10 @@
+const calc = require("./calc");
+const numbersToAdd = [3, 4, 10, 2];
+
+const result = calc.sum(numbersToAdd);
+console.log(`The result is: ${result}`);
+
+const _ = require("lodash");
+_.assign({ a: 1 }, { b: 2 }, { c: 3 });
+
+require("./fileread");
