df7bcad833d235af73d6fba4e23bd640c3ecf324
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-04 20:35:03 +0200

    first router

--- learn-node/routes/index.js
+++ learn-node/routes/index.js
@@ -3,7 +3,22 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  console.log('Hey !!');
+  // res.send('Hey! It works!');
+  const name = req.query.name || 'wes';
+  const cool = req.query.cool || true;
+
+  const wes = {
+    name,
+    cool,
+  };
+
+  res.json(wes);
 });
 
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+
+  res.send(reverse);
+});
 module.exports = router;
