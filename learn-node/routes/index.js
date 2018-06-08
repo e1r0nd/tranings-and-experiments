c3c8b47e1162c66c7d7285b4851fe04bf11ce721
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-08 21:17:50 +0200

    added Edit action

--- learn-node/routes/index.js
+++ learn-node/routes/index.js
@@ -6,7 +6,9 @@ const { catchErrors } = require('../handlers/errorHandlers');
 router.get('/', catchErrors(storeController.homePage));
 router.get('/stores', catchErrors(storeController.getStores));
 router.get('/add', storeController.addStore);
-router.post('/add', storeController.createStore);
+router.post('/add', catchErrors(storeController.createStore));
+router.post('/add/:id', catchErrors(storeController.updateStore));
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
 
 router.get('/hey', (req, res) => {
   console.log('Hey !!');
