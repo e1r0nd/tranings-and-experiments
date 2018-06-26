ddc0f03df73873bd37a4c00bb33bf8477c798ac2
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-27 01:23:12 +0200

    added Store owner logic

--- learn-node/controllers/storeController.js
+++ learn-node/controllers/storeController.js
@@ -8,12 +8,12 @@ const multerOptions = {
   storage: multer.memoryStorage(),
   fileFilter(req, file, next) {
     const isPhoto = file.mimetype.startsWith('image/');
-    if(isPhoto) {
+    if (isPhoto) {
       next(null, true);
     } else {
-      next({ message: 'That filetype isn\'t allowed!' }, false);
+      next({ message: "That filetype isn't allowed!" }, false);
     }
-  }
+  },
 };
 
 exports.homePage = (req, res) => {
@@ -43,8 +43,12 @@ exports.resize = async (req, res, next) => {
 };
 
 exports.createStore = async (req, res) => {
-  const store = await (new Store(req.body)).save();
-  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  req.body.author = req.user._id;
+  const store = await new Store(req.body).save();
+  req.flash(
+    'success',
+    `Successfully Created ${store.name}. Care to leave a review?`,
+  );
   res.redirect(`/store/${store.slug}`);
 };
 
@@ -54,11 +58,16 @@ exports.getStores = async (req, res) => {
   res.render('stores', { title: 'Stores', stores });
 };
 
+const confirmOwner = (store, user) => {
+  if (!store.author.equals(user._id)) {
+    throw Error('You are not an owner');
+  }
+};
 exports.editStore = async (req, res) => {
   // 1. Find the store given the ID
   const store = await Store.findOne({ _id: req.params.id });
   // 2. confirm they are the owner of the store
-  // TODO
+  confirmOwner(store, req.user);
   // 3. Render out the edit form so the user can update their store
   res.render('editStore', { title: `Edit ${store.name}`, store });
 };
@@ -69,15 +78,22 @@ exports.updateStore = async (req, res) => {
   // find and update the store
   const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
     new: true, // return the new store instead of the old one
-    runValidators: true
+    runValidators: true,
   }).exec();
-  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store →</a>`);
+  req.flash(
+    'success',
+    `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${
+      store.slug
+    }">View Store →</a>`,
+  );
   res.redirect(`/stores/${store._id}/edit`);
   // Redriect them the store and tell them it worked
 };
 
 exports.getStoreBySlug = async (req, res, next) => {
-  const store = await Store.findOne({ slug: req.params.slug });
+  const store = await Store.findOne({ slug: req.params.slug }).populate(
+    'author',
+  );
   if (!store) return next();
   res.render('store', { store, title: store.name });
 };
@@ -89,7 +105,6 @@ exports.getStoresByTag = async (req, res) => {
   const tagsPromise = Store.getTagsList();
   const storesPromise = Store.find({ tags: tagQuery });
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
-
 
   res.render('tag', { tags, title: 'Tags', tag, stores });
 };
--- learn-node/models/Store.js
+++ learn-node/models/Store.js
@@ -6,33 +6,40 @@ const storeSchema = new mongoose.Schema({
   name: {
     type: String,
     trim: true,
-    required: 'Please enter a store name!'
+    required: 'Please enter a store name!',
   },
   slug: String,
   description: {
     type: String,
-    trim: true
+    trim: true,
   },
   tags: [String],
   created: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   location: {
     type: {
       type: String,
-      default: 'Point'
+      default: 'Point',
     },
-    coordinates: [{
-      type: Number,
-      required: 'You must supply coordinates!'
-    }],
+    coordinates: [
+      {
+        type: Number,
+        required: 'You must supply coordinates!',
+      },
+    ],
     address: {
       type: String,
-      required: 'You must supply an address!'
-    }
+      required: 'You must supply an address!',
+    },
   },
-  photo: String
+  photo: String,
+  author: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: 'You must supply an author',
+  },
 });
 
 storeSchema.pre('save', async function(next) {
@@ -55,8 +62,8 @@ storeSchema.statics.getTagsList = function() {
   return this.aggregate([
     { $unwind: '$tags' },
     { $group: { _id: '$tags', count: { $sum: 1 } } },
-    { $sort: { count: -1 } }
+    { $sort: { count: -1 } },
   ]);
-}
+};
 
 module.exports = mongoose.model('Store', storeSchema);
