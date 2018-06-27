8c7af4bf329e4be8c640778d4777c8ca2bf2b4d2
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-27 22:51:23 +0200

    added Notes

--- nodejs-course/notes.js
+++ nodejs-course/notes.js
@@ -1,5 +1,24 @@
+const fs = require('fs');
+
 exports.addNote = (title, body) => {
-  console.log(title, body);
+  let notes = [];
+  const note = {
+    title,
+    body,
+  };
+
+  try {
+    const notesString = fs.readFileSync('notes-data.json');
+    notes = JSON.parse(notesString);
+    console.log(notes);
+  } catch (error) {}
+
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
 };
 
 exports.getNote = (title) => {
