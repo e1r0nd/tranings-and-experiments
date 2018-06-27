b9a659043a22e0a17a2950499518d7d9be33aa6c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-27 19:01:21 +0200

    adde note module

--- nodejs-course/app.js
+++ nodejs-course/app.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+console.log('Starting app...');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+const user = os.userInfo();
+
+fs.appendFile('greeting.txt', `Hello ${user.username || 'world'}!`, (err) => {
+  err && console.log('Unable to write to file.');
+});
+
+console.log(`Your age is ${notes.age}`);
+console.log(notes.sum(9, -2));
---
+++ nodejs-course/notes.js
@@ -0,0 +1,8 @@
+console.log('Starting notes.js');
+
+exports.age = 25;
+exports.addNote = () => {
+  console.log('addNote');
+  return 'New Note';
+};
+exports.sum = (a, b) => a + b;
