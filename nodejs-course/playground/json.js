dc1fc181011cef69c1d79a7488b190a27ebf607c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-27 22:27:45 +0200

    notes to json

---
+++ nodejs-course/playground/json.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+const obj = {
+  name: 'My name',
+};
+const stringObj = JSON.stringify(obj);
+console.log(stringObj);
+
+const personString = '{"name":"This name"}';
+const person = JSON.parse(personString);
+console.log(person);
+
+const originalNote = {
+  title: 'This title',
+  body: 'my body',
+};
+const originalNoteString = JSON.stringify(originalNote);
+fs.writeFileSync('notes.json', originalNoteString);
+
+const noteString = fs.readFileSync('notes.json');
+const note = JSON.parse(noteString);
+console.log(`title: ${note.title}`);
