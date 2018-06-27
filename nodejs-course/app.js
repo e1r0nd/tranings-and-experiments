85eef82d6448169693f5ca5e7dceda6aefc6706b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-27 19:11:54 +0200

    added lodash

--- nodejs-course/app.js
+++ nodejs-course/app.js
@@ -2,6 +2,7 @@ console.log('Starting app...');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 const user = os.userInfo();
@@ -12,3 +13,6 @@ fs.appendFile('greeting.txt', `Hello ${user.username || 'world'}!`, (err) => {
 
 console.log(`Your age is ${notes.age}`);
 console.log(notes.sum(9, -2));
+
+const filteredArray = _.uniq([1, 2, 3, 4, 1, 3, 5]);
+console.log(filteredArray);
