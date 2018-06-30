75b038a07b86b8d208c71dfc9f1c8fa4bd9a0c2c
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-06-30 22:55:26 +0200

    basics math

--- rust_code/src/main.rs
+++ rust_code/src/main.rs
@@ -2,4 +2,9 @@ mod rust1;
 
 fn main() {
     rust1::main1();
+
+    let number = 10;
+    let cubed = i32::pow(number, 3);
+    let cudeb_times_pi = cubed as f64 * std::f64::consts::PI;
+    println!("10^3 * PI = {}", cudeb_times_pi);
 }
--- rust_code/src/rust1.rs
+++ rust_code/src/rust1.rs
@@ -1,4 +1,21 @@
+use std::mem;
+
+fn foo_function() {
+    let a = "a String here";
+    println!("Print this: {}", a);
+}
+
 pub fn main1() {
     let bool_var = true;
     println!("Var: {}", bool_var);
+
+    foo_function();
+    let stack = 10;
+    let heap = Box::new(stack);
+    println!("stack={} heap={}", stack, heap);
+    println!(
+        "size of stack={}, size of heap={}",
+        mem::size_of_val(&stack),
+        mem::size_of_val(&heap)
+    );
 }
