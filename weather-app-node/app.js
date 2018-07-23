aa7eadebf9b7831c72ff23d6fee8de12ed9aedd2
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-07-23 23:00:13 +0200

    response handler

--- weather-app-node/app.js
+++ weather-app-node/app.js
@@ -1,14 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true,
+    },
+  })
+  .help()
+  .alias('help', 'h').argv;
 
+console.log(argv);
+
+const address = encodeURIComponent(argv.a) || 'Krucza%2073%20Wroclaw';
 request(
   {
-    url:
-      'http://maps.googleapis.com/maps/api/geocode/json?address=Krucza%2073%20Wroclaw',
+    url: `http://maps.googleapis.com/maps/api/geocode/json?address=${address}`,
     json: true,
   },
   (error, response, body) => {
     if (error) {
       console.error(error.message);
+      return;
+    } else if (body.status === 'ZERO_RESULTS') {
+      console.log('Unable to find that address.');
       return;
     }
 
