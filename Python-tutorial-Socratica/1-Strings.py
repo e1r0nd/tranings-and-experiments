8b90a0dda14da6cd88d48fd893ff4f461dde3ce1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-12-02 10:20:29 +0100

    Strings in python

---
+++ Python-tutorial-Socratica/1-Strings.py
@@ -0,0 +1,9 @@
+message = "I'm here. Meet me tonight."
+print(message)
+
+message2 = 'The clock strikes at midnight.'
+print(message2)
+
+message3 = """One more time.
+"I'm here to check it out" he said"""
+print(message3)
