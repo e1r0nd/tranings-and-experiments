e232079644397779b93242f65d1ed16a10bcafa7
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-12-06 21:36:58 +0100

    py: bool

---
+++ Python-tutorial-Socratica/3-Bool.py
@@ -0,0 +1,12 @@
+a = 3
+b = 5
+
+print("a < b = ", a < b)
+print(type(a < b))
+
+c = bool(a)
+print("c =", c)
+
+print("int(True) = ", int(True))
+print("5 + True = ", 5 + True)
+print("10 * False =", 10 * False)
