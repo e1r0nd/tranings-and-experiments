fc5dca33fc871a2b40890bb3d583243d3e376b5a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-12-19 20:23:54 +0100

    list.py

---
+++ Python-tutorial-Socratica/8-List.py
@@ -0,0 +1,13 @@
+primes = [2, 3, 2, 7, 11, 13]
+primes.append(17)
+primes.append(19)
+
+print(primes, primes[1], primes[-1])
+
+prime_slice = primes[3:5]
+print(prime_slice)
+
+numbers = [1, 2, 3]
+letters = ["a", "b", "c"]
+alpha_num = numbers + letters
+print(alpha_num)
