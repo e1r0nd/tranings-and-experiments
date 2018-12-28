826974e3ef17f88a4995b786509d4741b323f6b9
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2018-12-28 22:39:47 +0100

    added logger

--- Udemy-NodeJS-Andrew/node-web-server/server.js
+++ Udemy-NodeJS-Andrew/node-web-server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
-const app = express();
+const fs = require('fs');
 
+const app = express();
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maitance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
+
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', (text) => text.toUpperCase());
 
