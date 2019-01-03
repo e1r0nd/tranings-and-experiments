1640a12aad02c56e2804618279af5f87c63bb2b4
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-03 21:01:42 +0100

    module 2 - typescript

--- TypeScript-Udemy-Tutorial/script.js
+++ TypeScript-Udemy-Tutorial/script.js
@@ -1,19 +1,19 @@
 "use strict";
-function Greeter(greeting) {
-    this.greeting = greeting;
-}
-Greeter.prototype.greet = function () {
-    return 'Hello, ' + this.greeting;
-};
+// function Greeter(greeting: string) {
+//   // this.greeting = greeting;
+// }
+// Greeter.prototype.greet = function() {
+//   return 'Hello, ' + this.greeting;
+// };
 // Oops, we're passing an object when we want a string. This will print
 // "Hello, [object Object]" instead of "Hello, world" without error.
-var greeter = new Greeter('world');
-var button = document.createElement('button');
-button.textContent = 'Say Hello';
-button.onclick = function () {
-    alert(greeter.greet());
-};
-document.body.appendChild(button);
+// let greeter = new Greeter('world');
+// let button = document.createElement('button');
+// button.textContent = 'Say Hello';
+// button.onclick = function() {
+//   alert(greeter.greet());
+// };
+// document.body.appendChild(button);
 console.log('It works!');
 var str = 'str';
 // str = 28; <- cannot be Number
@@ -41,3 +41,49 @@ var Color;
 })(Color || (Color = {}));
 var myColor = Color.Green;
 console.log(myColor); // -> 100
+// functions
+function returnMyAge() {
+    return myAge;
+}
+console.log(returnMyAge());
+function hello() {
+    console.log('Hello!');
+}
+function multiplier(a, b) {
+    return a * b;
+}
+console.log(multiplier(2, 3));
+// function types
+var myMultiplier;
+// myMultiplier = hello; <- not valid
+myMultiplier = multiplier;
+console.log(myMultiplier(3, 4));
+// Objects
+var userData = {
+    name: 'Some',
+    age: 42,
+};
+// userData = { <- cannot reassign properties ot thier types
+//   a: 1,
+//   b: 2,
+// };
+// complex object
+var complex = {
+    data: [3, 2, 1],
+    output: function (all) {
+        return this.data;
+    },
+};
+var complex1 = {
+    data: [3, 2, 1],
+    output: function (all) {
+        return this.data;
+    },
+};
+// union types
+var yourNumber = 42;
+yourNumber = '42';
+// yourNumber = true; <- not valid
+if (typeof yourNumber == 'string') {
+    console.log(yourNumber);
+}
--- TypeScript-Udemy-Tutorial/script.ts
+++ TypeScript-Udemy-Tutorial/script.ts
@@ -1,22 +1,22 @@
-function Greeter(greeting: string) {
-  this.greeting = greeting;
-}
+// function Greeter(greeting: string) {
+//   // this.greeting = greeting;
+// }
 
-Greeter.prototype.greet = function() {
-  return 'Hello, ' + this.greeting;
-};
+// Greeter.prototype.greet = function() {
+//   return 'Hello, ' + this.greeting;
+// };
 
 // Oops, we're passing an object when we want a string. This will print
 // "Hello, [object Object]" instead of "Hello, world" without error.
-let greeter = new Greeter('world');
+// let greeter = new Greeter('world');
 
-let button = document.createElement('button');
-button.textContent = 'Say Hello';
-button.onclick = function() {
-  alert(greeter.greet());
-};
+// let button = document.createElement('button');
+// button.textContent = 'Say Hello';
+// button.onclick = function() {
+//   alert(greeter.greet());
+// };
 
-document.body.appendChild(button);
+// document.body.appendChild(button);
 console.log('It works!');
 
 let str = 'str';
@@ -49,3 +49,70 @@ enum Color {
 }
 let myColor: Color = Color.Green;
 console.log(myColor); // -> 100
+
+// functions
+function returnMyAge(): number {
+  return myAge;
+}
+console.log(returnMyAge());
+
+function hello(): void {
+  console.log('Hello!');
+}
+
+function multiplier(a: number, b: number) {
+  return a * b;
+}
+console.log(multiplier(2, 3));
+
+// function types
+let myMultiplier: (x: number, y: number) => number;
+// myMultiplier = hello; <- not valid
+myMultiplier = multiplier;
+console.log(myMultiplier(3, 4));
+
+function neverReturns(): never {
+  throw new Error('oops');
+}
+// Objects
+let userData: { name: string; age: number } = {
+  name: 'Some',
+  age: 42,
+};
+// userData = { <- cannot reassign properties ot thier types
+//   a: 1,
+//   b: 2,
+// };
+
+// complex object
+let complex: { data: number[]; output: (all: boolean) => number[] } = {
+  data: [3, 2, 1],
+  output: function(all: boolean): number[] {
+    return this.data;
+  },
+};
+
+// type alias
+type ComplexType = { data: number[]; output: (all: boolean) => number[] };
+let complex1: ComplexType = {
+  data: [3, 2, 1],
+  output: function(all: boolean): number[] {
+    return this.data;
+  },
+};
+
+// union types
+let yourNumber: number | string = 42;
+yourNumber = '42';
+// yourNumber = true; <- not valid
+
+if (typeof yourNumber == 'string') {
+  console.log(yourNumber);
+}
+
+// Nullable Types
+let canBeNull: number | null = 12;
+canBeNull = null;
+let isUndefined;
+isUndefined = null; // <- can be null
+isUndefined = 42; // because it's defined as 'any'
