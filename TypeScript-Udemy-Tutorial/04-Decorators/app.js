ab58b56586a869fb77c593c66a265c059bdd6dd3
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-20 15:13:45 +0100

    more TS decorators

--- TypeScript-Udemy-Tutorial/04-Decorators/app.js
+++ TypeScript-Udemy-Tutorial/04-Decorators/app.js
@@ -5,6 +5,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 console.log('--- Decorators ---');
 function logged(constructorFn) {
     console.log(constructorFn);
@@ -18,4 +21,87 @@ var Person = /** @class */ (function () {
     ], Person);
     return Person;
 }());
+// Factory
+function logging(value) {
+    return value ? logged : null;
+}
+// @logging(false)
+// class Car {}
+// Advanced
+function printable(constructorFn) {
+    constructorFn.prototype.print = function () {
+        console.log(this);
+    };
+}
+function echo(constructorFn) {
+    constructorFn.prototype.echo = function () {
+        console.log(this);
+    };
+}
+var Plant = /** @class */ (function () {
+    function Plant() {
+        this.name = 'Green Plant';
+    }
+    Plant = __decorate([
+        printable,
+        echo,
+        logged
+    ], Plant);
+    return Plant;
+}());
+var plant = new Plant();
+plant.print();
+// Method Descorator
+// -- This works in TSv1.8 but doesn't in v3.2. Not sure why.
+// function editable(value: boolean) {
+//   return function(
+//     target: any,
+//     propName: string,
+//     descriptor: PropertyDescriptor,
+//   ) {
+//     descriptor.writable = value;
+//   };
+// }
+// class Project {
+//   projectName: string;
+//   constructor(name: string) {
+//     this.projectName = name;
+//   }
+//   @editable(false)
+//   calcBudget() {
+//     console.log(1000);
+//   }
+// }
+// const project = new Project('New Project');
+// project.calcBudget();
+// project.calcBudget = function() {
+//   console.log(2000);
+// };
+// project.calcBudget();
+// Parameter Decorator
+function printInfo(target, methodName, paramIndex) {
+    console.log('Target:', target);
+    console.log('Method Name:', methodName);
+    console.log('Parameter Index:', paramIndex);
+}
+var Course = /** @class */ (function () {
+    function Course(name) {
+        this.name = name;
+    }
+    Course.prototype.printStudentNumber = function (mode, printAll) {
+        if (printAll) {
+            console.log(mode, 1999);
+        }
+        else {
+            console.log(mode, 100);
+        }
+    };
+    __decorate([
+        __param(1, printInfo)
+    ], Course.prototype, "printStudentNumber", null);
+    return Course;
+}());
+var course = new Course('New Course');
+course.printStudentNumber('', true);
+course.printStudentNumber('', false);
 //# sourceMappingURL=app.js.map
--- TypeScript-Udemy-Tutorial/04-Decorators/app.ts
+++ TypeScript-Udemy-Tutorial/04-Decorators/app.ts
@@ -16,5 +16,76 @@ function logging(value: boolean) {
   return value ? logged : null;
 }
 
-@logging(false)
-class Car {}
+// @logging(false)
+// class Car {}
+
+// Advanced
+function printable(constructorFn: Function) {
+  constructorFn.prototype.print = function() {
+    console.log(this);
+  };
+}
+function echo(constructorFn: Function) {
+  constructorFn.prototype.echo = function() {
+    console.log(this);
+  };
+}
+@printable
+@echo
+@logged
+class Plant {
+  name = 'Green Plant';
+}
+const plant = new Plant();
+(<any>plant).print();
+
+// Method Descorator
+// -- This works in TSv1.8 but doesn't in v3.2. Not sure why.
+// function editable(value: boolean) {
+//   return function(
+//     target: any,
+//     propName: string,
+//     descriptor: PropertyDescriptor,
+//   ) {
+//     descriptor.writable = value;
+//   };
+// }
+// class Project {
+//   projectName: string;
+//   constructor(name: string) {
+//     this.projectName = name;
+//   }
+//   @editable(false)
+//   calcBudget() {
+//     console.log(1000);
+//   }
+// }
+// const project = new Project('New Project');
+// project.calcBudget();
+// project.calcBudget = function() {
+//   console.log(2000);
+// };
+// project.calcBudget();
+
+// Parameter Decorator
+function printInfo(target: any, methodName: string, paramIndex: number) {
+  console.log('Target:', target);
+  console.log('Method Name:', methodName);
+  console.log('Parameter Index:', paramIndex);
+}
+class Course {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  printStudentNumber(mode: string, @printInfo printAll: boolean) {
+    if (printAll) {
+      console.log(mode, 1999);
+    } else {
+      console.log(mode, 100);
+    }
+  }
+}
+const course = new Course('New Course');
+course.printStudentNumber('', true);
+course.printStudentNumber('', false);
