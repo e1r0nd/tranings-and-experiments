708d32d2ee7308c9e252f3573fc05e16fa7036d1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-20 15:17:39 +0100

    cpp project - init

---
+++ Cpp-First-Tutorial/Readme.md
@@ -0,0 +1,3 @@
+# Уроки C++ с нуля
+
+https://www.youtube.com/watch?v=llovDtQN_u4&list=PL0lO_mIqDDFXNfqIL9PHQM7Wg_kOtDZsW&index=2
---
+++ Cpp-First-Tutorial/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main(int argc, const char *argv[])
+{
+    // insert code here...
+    std::cout << "Hello, world!\n";
+    return 0;
+}
