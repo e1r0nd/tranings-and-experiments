17e59bb33f5020388812843e28d5c2134b0847c4
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-21 14:25:56 +0100

    node async tests

--- Udemy-NodeJS-Andrew/node-tests/utils/utils.js
+++ Udemy-NodeJS-Andrew/node-tests/utils/utils.js
@@ -1,5 +1,21 @@
 module.exports.add = (a, b) => {
   return a + b;
 };
+module.exports.asyncAdd = (a, b, callback) => {
+  setTimeout(() => {
+    callback(a + b);
+  }, 1000);
+};
 
 module.exports.square = (x) => x * x;
+module.exports.asynSquare = (x, callback) => {
+  setTimeout(() => {
+    callback(x * x);
+  }, 1000);
+};
+
+module.exports.setName = (user, fullName) => {
+  const names = fullName.split(' ');
+  [user.firstName, user.lastName] = names;
+  return user;
+};
--- Udemy-NodeJS-Andrew/node-tests/utils/utils.test.js
+++ Udemy-NodeJS-Andrew/node-tests/utils/utils.test.js
@@ -5,7 +5,19 @@ it('Should add two numbers', () => {
   const res = utils.add(3, 2);
   assert.equal(res, 5);
 });
+it('Should async add two numbers', (done) => {
+  utils.asyncAdd(4, 3, (sum) => {
+    assert.equal(sum, 7);
+    done();
+  });
+});
 
 it('Should square a number', () => {
   assert.equal(utils.square(2), 4);
+});
+it('Should async square a number', (done) => {
+  utils.asynSquare(2, (res) => {
+    assert.equal(res, 4);
+    done();
+  });
 });
