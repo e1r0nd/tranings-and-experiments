e8a03f3bfa952cf8b13deddffe0b27015faaad8d
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-01-23 15:14:56 +0100

    added a NProgress bar

--- _React-Redux/sick-fits--wesbos-advanced-react/frontend/components/Header.js
+++ _React-Redux/sick-fits--wesbos-advanced-react/frontend/components/Header.js
@@ -1,6 +1,19 @@
 import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Link from 'next/link';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import Nav from './Nav';
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  console.error('Loading a router error!');
+  NProgress.done();
+};
 
 const Logo = styled.h1`
   font-size: 4rem;
