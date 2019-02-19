4cb007d0d26721565e9df80bd5329ceb52e9fda1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-02-19 16:07:33 +0100

    connected to mLab server

---
+++ _NodeJS/Udemy-NodeJS-Andrew/node-todo-api/playground/mongodb-connect.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+console.log(
+  `${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}`,
+);
+MongoClient.connect(
+  `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${
+    process.env.DB_HOST
+  }:61024/node-todo-api`,
+  (err, db) => {
+    if (err) {
+      return console.log('Unable to connect to MongoDB Server');
+    }
+    console.log('Connected to MongoDB Server');
+
+    db.collection('Todos').insertOne(
+      {
+        text: 'Something to do',
+        completed: false,
+      },
+      (err, result) => {
+        if (err) {
+          return console.log('Unable to insert todo', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      },
+    );
+
+    db.collection('Users').insertOne(
+      {
+        name: 'Andrew',
+        age: '12',
+        location: 'false',
+      },
+      (err, result) => {
+        if (err) {
+          return console.log('Unable to insert User', err);
+        }
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      },
+    );
+
+    db.close();
+  },
+);
