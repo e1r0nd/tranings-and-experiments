1bcf41d577dee5e6c24a179b07e4c61f8d8f496b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-03-07 15:23:31 +0100

    refactoring

--- _NodeJS/Udemy-NodeJS-Andrew/note-app/app.js
+++ _NodeJS/Udemy-NodeJS-Andrew/note-app/app.js
@@ -19,45 +19,85 @@ const bodyOptions = {
   alias: 'b',
 };
 const argv = yargs
-  .command('add', 'Add a new note', {
-    title: titleOptions,
-    body: bodyOptions,
+  .command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string',
+        titleOptions,
+      },
+      body: {
+        describe: 'Note body',
+        demandOption: true,
+        type: 'string',
+        bodyOptions,
+      },
+    },
+    handler: function(argv) {
+      const note = notes.addNote(argv.title, argv.body);
+      if (_.isObject(note)) {
+        console.log(chalk.green('Note created'));
+        notes.logNote(note);
+      } else if (note) {
+        console.log(note);
+      }
+    },
   })
-  .command('list', 'List all notes')
-  .command('read', 'Read a new note by title', {
-    title: titleOptions,
+  .command({
+    command: 'list',
+    describe: 'List all notes',
+    handler: function(argv) {
+      const allNotes = notes.getAll();
+      console.log(`Found ${chalk.yellow(allNotes.length)} notes:`);
+      allNotes.forEach((note) => notes.logNote(note));
+    },
   })
-  .command('remove', 'Remove a new note by title', {
-    title: titleOptions,
+  .command({
+    command: 'read',
+    describe: 'Read a new note by title',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string',
+        titleOptions,
+      },
+    },
+    handler: function(argv) {
+      const note = notes.getNote(argv.title);
+      if (_.isObject(note)) {
+        console.log('The Note');
+        notes.logNote(note);
+      } else {
+        console.log(chalk.red('Cannot find this note'));
+      }
+    },
+  })
+  .command({
+    command: 'remove',
+    describe: 'Remove a new note by title',
+    builder: {
+      title: {
+        describe: 'Note title',
+        demandOption: true,
+        type: 'string',
+        titleOptions,
+      },
+    },
+    handler: function(argv) {
+      const noteRemoved = notes.removeNote(argv.title);
+      const message = noteRemoved
+        ? `Note "${chalk.red(argv.title)} is removed`
+        : 'Note was not removed';
+      console.log(message);
+    },
   }).argv;
 const command = (argv._[0] && argv._[0].toLowerCase()) || '?';
-
-if (command === 'add') {
-  const note = notes.addNote(argv.title, argv.body);
-  if (_.isObject(note)) {
-    console.log(chalk.green('Note created'));
-    notes.logNote(note);
-  } else if (note) {
-    console.log(note);
-  }
-} else if (command === 'list') {
-  const allNotes = notes.getAll();
-  console.log(`Found ${chalk.yellow(allNotes.length)} notes:`);
-  allNotes.forEach((note) => notes.logNote(note));
-} else if (command === 'read') {
-  const note = notes.getNote(argv.title);
-  if (_.isObject(note)) {
-    console.log('The Note');
-    notes.logNote(note);
-  } else {
-    console.log(chalk.red('Cannot find this note'));
-  }
-} else if (command === 'remove') {
-  const noteRemoved = notes.removeNote(argv.title);
-  const message = noteRemoved
-    ? `Note "${chalk.red(argv.title)} is removed`
-    : 'Note was not removed';
-  console.log(message);
-} else {
+if (command === '?') {
   console.log(`Usage: app.js ${chalk.green('[add | list | remove | --help]')}`);
 }
+
+yargs.parse();
