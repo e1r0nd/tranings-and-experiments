481a52918eeee8e4f22ce8e0c68c5b77f8e3ea48
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-03-23 22:33:13 +0100

    django start

---
+++ _Python/Python-Django-Brad-Traversy/sanbox/variables.py
@@ -0,0 +1,25 @@
+"""
+Variables
+"""
+
+x = 1  # int
+y = 2.5  # float
+name = "cool"  # string
+is_cool = True  # bool
+
+# Multipla assignments
+a, b, txt = (1, 1.2, "oops")
+
+print(y, a, txt)
+
+# Basic math
+sum = x + y + a + b
+print("Sum = ", sum)
+
+# Check type
+print(type(is_cool))
+
+# Casting
+x = str(x)
+print(x + txt)
+
