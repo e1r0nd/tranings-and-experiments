7cb3bff8b26b9b0f7841d826e37ca58f46a1fdb1
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-04-04 21:28:59 +0200

    clean up

--- _NodeJS/Udemy-NodeJS-Andrew/web-server/public/js/script.js
+++ _NodeJS/Udemy-NodeJS-Andrew/web-server/public/js/script.js
@@ -4,19 +4,17 @@ formAddress.addEventListener('submit', (e) => {
   e.preventDefault();
   if (formAddress.address.value) {
     responseBlock.textContent = 'Loading...';
-    fetch(`//localhost:3000/weather?address=${formAddress.address.value}`).then(
-      (response) => {
-        response.json().then((data) => {
-          if (data.error) {
-            responseBlock.textContent = data.error;
-          } else {
-            responseBlock.innerHTML = `<p><b>Location:</b> ${
-              data.location
-            }</p><p><b>Forecast:</b> ${data.forecast}`;
-          }
-        });
-      },
-    );
+    fetch(`/weather?address=${formAddress.address.value}`).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          responseBlock.textContent = data.error;
+        } else {
+          responseBlock.innerHTML = `<p><b>Location:</b> ${
+            data.location
+          }</p><p><b>Forecast:</b> ${data.forecast}`;
+        }
+      });
+    });
   } else {
     responseBlock.textContent = 'Please provide an address';
   }
