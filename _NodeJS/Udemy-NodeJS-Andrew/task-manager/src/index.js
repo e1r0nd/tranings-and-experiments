9bcea5d34a3641e31abf73ff9492c8f689cd7d85
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-04-26 18:14:30 +0200

    added read API

--- _NodeJS/Udemy-NodeJS-Andrew/task-manager/src/index.js
+++ _NodeJS/Udemy-NodeJS-Andrew/task-manager/src/index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
+/* Users Requests */
 app.post('/users', (req, res) => {
   const user = new User(req.body);
   user
@@ -19,12 +20,56 @@ app.post('/users', (req, res) => {
       res.status(400).send(error);
     });
 });
+app.get('/users', (req, res) => {
+  User.find({})
+    .then((users) => res.send(users))
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+app.get('/users/:id', (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (user) {
+        return res.send(user);
+      } else {
+        return res.status(404).send('User not found!');
+      }
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+
+/* Tasks Requests */
 app.post('/tasks', (req, res) => {
   const task = new Task(req.body);
   task
     .save()
     .then(() => {
       res.status(201).send(task);
+    })
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+app.get('/tasks', (req, res) => {
+  Task.find({})
+    .then((tasks) => res.send(tasks))
+    .catch((error) => {
+      res.status(400).send(error);
+    });
+});
+app.get('/tasks/:id', (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (task) {
+        return res.send(task);
+      } else {
+        return res.status(404).send('Task not found!');
+      }
     })
     .catch((error) => {
       res.status(400).send(error);
