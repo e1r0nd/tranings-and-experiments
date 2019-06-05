b792394867662688bb33c3418c11f4565d1619d3
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-06-05 13:11:57 +0200

    added Frontend Skeleton

---
+++ _Python/Fullstack-Python-React/leadmanager/frontend/src/components/leads/Form.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Form extends Component {
+  render() {
+    return <h1>Add lead form</h1>;
+  }
+}
+
+export default Form;
---
+++ _Python/Fullstack-Python-React/leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class Leads extends Component {
+  render() {
+    return <h1>Leads list</h1>;
+  }
+}
+
+export default Leads;
