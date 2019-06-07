da1a557c8df8fb3ce386d4299a66517374ed715a
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-06-07 22:27:53 +0200

    added "Send location"

--- _NodeJS/Udemy-NodeJS-Andrew/chat-app/public/js/chat.js
+++ _NodeJS/Udemy-NodeJS-Andrew/chat-app/public/js/chat.js
@@ -16,3 +16,16 @@ document.querySelector('#increment').addEventListener('click', () => {
   console.log('clicked');
   socket.emit('increment');
 });
+
+document.querySelector('#send-location').addEventListener('click', () => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser.');
+  }
+  navigator.geolocation.getCurrentPosition(position => {
+    console.log(position);
+    socket.emit('sendLocation', {
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+    });
+  });
+});
