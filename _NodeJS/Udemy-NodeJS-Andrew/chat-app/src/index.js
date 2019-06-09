3ba71b80624dd8319b86b17b86673c8601276f2b
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2019-06-09 21:38:19 +0200

    added aknowledgements

--- _NodeJS/Udemy-NodeJS-Andrew/chat-app/src/index.js
+++ _NodeJS/Udemy-NodeJS-Andrew/chat-app/src/index.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io');
+const socketIo = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
-const io = socketio(server);
+const io = socketIo(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -17,8 +18,13 @@ io.on('connection', socket => {
   console.log('New websocket connection:', count);
   socket.emit('message', 'Welcome!');
   socket.broadcast.emit('message', 'A new user has joined!');
-  socket.on('sendMessage', message => {
+  socket.on('sendMessage', (message, callback) => {
+    const filter = new Filter();
+    if (filter.isProfane(message)) {
+      return callback('Profanity is not allowed.');
+    }
     io.emit('message', message);
+    callback();
   });
 
   socket.emit('countUpdated', count);
@@ -31,11 +37,12 @@ io.on('connection', socket => {
     io.emit('message', 'A user has left!');
   });
 
-  socket.on('sendLocation', coords => {
+  socket.on('sendLocation', (coords, callback) => {
     io.emit(
       'message',
       `https://google.com/maps?q=${coords.latitude},${coords.longitude}`,
     );
+    callback();
   });
 });
 
