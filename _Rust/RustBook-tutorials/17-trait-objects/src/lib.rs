8668f71f76f1b8298c1fd5e505e0d4a4f30127bb
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-02-16 06:05:54 +0100

    Started some OOP

--- _Rust/RustBook-tutorials/17-trait-objects/src/lib.rs
+++ _Rust/RustBook-tutorials/17-trait-objects/src/lib.rs
@@ -16,3 +16,15 @@ where
         }
     }
 }
+
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // code to actually draw a button
+    }
+}
