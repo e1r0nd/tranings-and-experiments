a9715eda71bac2f9fd3a4b4c100f683ac1abf6db
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-03-03 18:14:06 +0100

    destruct

--- _Rust/RustBook-tutorials/18-desctruct/src/main.rs
+++ _Rust/RustBook-tutorials/18-desctruct/src/main.rs
@@ -59,4 +59,10 @@ fn main() {
 
     let ((feet, inches), Point { x, y }, length) = ((3, 10), Point { x: 3, y: -10 }, 10);
     println!("inches = {}", length);
+
+    foo(3, 4);
+}
+
+fn foo(_: i32, y: i32) {
+    println!("This code only uses the y parameter: {}", y);
 }
