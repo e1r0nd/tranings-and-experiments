5f48dc09359c7b2e288be5bfc7cf4499a53aef66
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-03-22 20:05:04 +0100

    Added python types

--- _Python/microblog/app/routes.py
+++ _Python/microblog/app/routes.py
@@ -1,3 +1,4 @@
+from typing import Optional, Union, Dict, Sequence
 from flask import render_template
 from app import app
 
@@ -5,9 +6,27 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {"username": "Miguel"}
-    posts = [
-        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
-        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
-    ]
+    user = mock_user(True)
+    posts = mock_posts(True)
+
     return render_template("index.html", title="Home", user=user, posts=posts)
+
+
+def mock_user(user: bool) -> Union[Dict[str, str], None]:
+    if user:
+        return {"username": "Miguel"}
+    else:
+        None
+
+
+def mock_posts(posts: bool) -> Optional[Sequence]:
+    if posts:
+        return [
+            {"author": {"username": "John"}, "body": "Beautiful day in Portland!!!"},
+            {
+                "author": {"username": "Susan"},
+                "body": "The Avengers movie was so cool!",
+            },
+        ]
+    else:
+        None
