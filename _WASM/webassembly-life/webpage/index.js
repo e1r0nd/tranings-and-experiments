7e80b9345be47605e84d3dcd9ca01b910b822850
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-04-18 14:11:18 +0200

    Added interactivity!

--- _WASM/webassembly-life/webpage/index.js
+++ _WASM/webassembly-life/webpage/index.js
@@ -19,15 +19,47 @@ canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext("2d");
-
+let animationId = null;
+let timeoutId = null;
+let framesPerSecond = 10;
+// This function is the same as before, except the
+// result of `requestAnimationFrame` is assigned to
+// `animationId`.
 const renderLoop = () => {
-  universe.tick();
-
   drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  universe.tick();
+
+  timeoutId = setTimeout(() => {
+    animationId = requestAnimationFrame(renderLoop);
+  }, 1000 / framesPerSecond);
 };
+const isPaused = () => {
+  return animationId === null;
+};
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+  clearTimeout(timeoutId);
+  timeoutId = null;
+};
+
+playPauseButton.addEventListener("click", (event) => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
 
 const drawGrid = () => {
   ctx.beginPath();
@@ -76,6 +108,30 @@ const drawCells = () => {
   ctx.stroke();
 };
 
+canvas.addEventListener("click", (event) => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+});
+
+const framesPerSecondInput = document.getElementById("framesPerSecond");
+framesPerSecondInput.addEventListener("change", (e) => {
+  framesPerSecond = e.target.value;
+});
+
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+// This used to be `requestAnimationFrame(renderLoop)`.
+play();
