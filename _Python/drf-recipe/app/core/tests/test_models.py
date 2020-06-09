73a9eb98caa56f41567d22549f8f799e6db30c12
e1r0nd <a.merezhanyi@gmail.com>
2020-06-10 00:18:18 +0200

    Added custom user model

--- _Python/drf-recipe/app/core/tests/test_models.py
+++
@@ -1,7 +0,0 @@
-from django.test import TestCase
-from django.contrib.auth import get_user_model
-
-
-class ModelTests(TestCase):
-    def test_create_user_email_successful(self):
-        """Test creating a new user email with an email is successful"""
