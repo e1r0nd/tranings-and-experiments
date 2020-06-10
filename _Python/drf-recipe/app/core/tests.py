47c2944fe75fcea21e42636b412e186fa220de33
e1r0nd <a.merezhanyi@gmail.com>
2020-06-10 18:02:11 +0200

    Configured postgres DB

--- _Python/drf-recipe/app/core/tests.py
+++ _Python/drf-recipe/app/core/tests.py
@@ -1,6 +1,10 @@
+from unittest.mock import patch
+
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 from django.urls import reverse
+from django.core.management import call_command
+from django.db.utils import OperationalError
 
 
 class ModelTests(TestCase):
@@ -8,60 +12,51 @@ class ModelTests(TestCase):
         """Test creating a new user email with an email is successful"""
         email = "test@london.com"
         password = "password123"
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+        user = get_user_model().objects.create_user(email=email, password=password)
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
     def test_new_user_email_normalized(self):
         """Test the email for a new user is normalized"""
         email = "test@LONDON.com"
-        user = get_user_model().objects.create_user(email, 'test124')
+        user = get_user_model().objects.create_user(email, "test124")
 
         self.assertEqual(user.email, email.lower())
-        
+
     def test_new_user_invalid_email(self):
         """Test creating user with no email raises error"""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user(None, 'test123')
+            get_user_model().objects.create_user(None, "test123")
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser(
-            'test@london.com',
-            'test123'
-        )
+        user = get_user_model().objects.create_superuser("test@london.com", "test123")
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
 
 class AdminSiteTests(TestCase):
     def setUp(self):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
-            email='admin@london.com',
-            password='password123'
+            email="admin@london.com", password="password123"
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
-            email='user@london.com',
-            password='password123',
-            name="Test User"
+            email="user@london.com", password="password123", name="Test User"
         )
 
     def test_user_listed(self):
         """Test that users are listed on user page"""
-        url = reverse('admin:core_user_changelist')
+        url = reverse("admin:core_user_changelist")
         res = self.client.get(url)
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
 
-    
     def test_user_change_page(self):
         """ Test that user edit page works"""
-        url = reverse('admin:core_user_change', args=[self.user.id])
+        url = reverse("admin:core_user_change", args=[self.user.id])
         # /admin/core/user/1
         res = self.client.get(url)
 
@@ -69,7 +64,24 @@ class AdminSiteTests(TestCase):
 
     def test_create_user_page(self):
         """ Test that create user page works"""
-        url = reverse('admin:core_user_add')
+        url = reverse("admin:core_user_add")
         res = self.client.get(url)
 
         self.assertEqual(res.status_code, 200)
+
+
+class CommandTests(TestCase):
+    def test_wait_for_db_ready(self):
+        """Test waiting for db when db is available"""
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
+            gi.return_value = True
+            call_command("wait_for_db")
+            self.assertEqual(gi.call_count, 1)
+
+    @patch("time.sleep", return_value=True)
+    def test_wait_for_db(self, ts):
+        """Test waiting for db"""
+        with patch("django.db.utils.ConnectionHandler.__getitem__") as gi:
+            gi.side_effect = [OperationalError] * 5 + [True]
+            call_command("wait_for_db")
+            self.assertEqual(gi.call_count, 6)
