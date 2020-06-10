c270fdfb391fa292265882adfa00d9ae8a9be36c
e1r0nd <a.merezhanyi@gmail.com>
2020-06-10 23:20:17 +0200

    endpoint for users

--- _Python/drf-recipe/app/app/settings.py
+++ _Python/drf-recipe/app/app/settings.py
@@ -31,13 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'core.apps.CoreConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'core',
+    'users',
 ]
 
 MIDDLEWARE = [
