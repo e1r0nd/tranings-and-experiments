d63518e5198f162912b1f161b669989cce562ca8
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-06-20 07:12:17 +0200

    Start tweets

---
+++ _Python/tweet2/tweets/views.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse, Http404, JsonResponse
+from django.shortcuts import render
+
+from .models import Tweet
+
+
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content} for x in qs]
+    data = {"response": tweets_list}
+    status = 200
+
+    return JsonResponse(data, status=status)
+
+
+def tweet_detail_view(request, tweet_id, *arg, **kwargs):
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+    except:
+        data["message"] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
