76c6b048520abb76d3be3ef0bfe797fc366ae8c3
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-07-08 23:00:00 +0200

    start

---
+++ _TypeScript/Udemy/script.js
@@ -0,0 +1,7 @@
+function add(n1, n2) {
+    return n1 + n2;
+}
+var a = 2;
+var b = 5.5;
+var result = add(a, b);
+console.log(result);
---
+++ _TypeScript/Udemy/script.ts
@@ -0,0 +1,8 @@
+function add(n1: number, n2: number) {
+  return n1 + n2;
+}
+const a = 2;
+const b = 5.5;
+
+const result = add(a, b);
+console.log(result);
