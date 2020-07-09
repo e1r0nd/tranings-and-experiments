0bae288b68d51e75bf8bc2367bbc36acfc9ebfdc
Anatol Merezhanyi <a.merezhanyi@gmail.com>
2020-07-09 10:59:48 +0200

    added Union Types

--- _TypeScript/Udemy/1-Types-Basics/script.js
+++ _TypeScript/Udemy/1-Types-Basics/script.js
@@ -1,6 +1,5 @@
-/** Objects */
-var person = {};
 /** Basics */
+console.log(">>> Basics");
 function add(n1, n2) {
     return n1 + n2;
 }
@@ -8,3 +7,41 @@ var a = 2;
 var b = 5.5;
 var result = add(a, b);
 console.log(result);
+/** Objects */
+console.log(">>> Objects");
+var PersonPosition;
+(function (PersonPosition) {
+    PersonPosition[PersonPosition["ADMIN"] = 0] = "ADMIN";
+    PersonPosition[PersonPosition["READ_ONLY"] = 1] = "READ_ONLY";
+    PersonPosition[PersonPosition["AUTHOR"] = 2] = "AUTHOR";
+})(PersonPosition || (PersonPosition = {}));
+var person = {
+    name: "iam",
+    age: 42,
+    hobbies: ["sports", "cooking"],
+    role: [2, "author"],
+    position: PersonPosition.ADMIN
+};
+console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+    // console.log(hobby.map()); >>> TypeError
+}
+person.role = [0, "admin"];
+/** Union Types */
+console.log(">>> Union Types");
+function combine(inp1, inp2) {
+    var result;
+    if (typeof inp1 === "number" && typeof inp2 === "number") {
+        result = inp1 + inp2;
+    }
+    else {
+        result = String(inp1) + String(inp2);
+    }
+    return result;
+}
+var combinedNumbers = combine(40, 2);
+console.log("Combined numbers: " + combinedNumbers);
+var combinedStrings = combine("foo", "bar");
+console.log("Combined strings: " + combinedStrings);
--- _TypeScript/Udemy/1-Types-Basics/script.ts
+++ _TypeScript/Udemy/1-Types-Basics/script.ts
@@ -1,7 +1,6 @@
-/** Objects */
-const person = {};
-
 /** Basics */
+console.log(">>> Basics");
+
 function add(n1: number, n2: number) {
   return n1 + n2;
 }
@@ -10,3 +9,52 @@ const b = 5.5;
 
 const result = add(a, b);
 console.log(result);
+
+/** Objects */
+console.log(">>> Objects");
+
+enum PersonPosition {
+  ADMIN = 0,
+  READ_ONLY = 1,
+  AUTHOR = 2,
+}
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+  position: PersonPosition;
+} = {
+  name: "iam",
+  age: 42,
+  hobbies: ["sports", "cooking"],
+  role: [2, "author"], // Tuple
+  position: PersonPosition.ADMIN, // Enum
+};
+
+console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+  // console.log(hobby.map()); >>> TypeError
+}
+
+person.role = [0, "admin"];
+
+/** Union Types */
+console.log(">>> Union Types");
+
+function combine(inp1: number | string, inp2: number | string) {
+  let result;
+  if (typeof inp1 === "number" && typeof inp2 === "number") {
+    result = inp1 + inp2;
+  } else {
+    result = String(inp1) + String(inp2);
+  }
+  return result;
+}
+
+const combinedNumbers = combine(40, 2);
+console.log(`Combined numbers: ${combinedNumbers}`);
+
+const combinedStrings = combine("foo", "bar");
+console.log(`Combined strings: ${combinedStrings}`);
