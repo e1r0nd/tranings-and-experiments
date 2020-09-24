218a1f130852ccf4e0b88080e8cfaf58f069342d
e1r0nd <a.merezhanyi@gmail.com>
2020-09-24 21:01:15 +0200

    some examples

--- Java/java-se-beginner-course/beginner-course/src/start/App.java
+++ Java/java-se-beginner-course/beginner-course/src/start/App.java
@@ -1,7 +1,51 @@
 package start;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 public class App {
+    private static void refTypes() {
+        int a = 10;
+        int b = a;
+        System.out.println(b);
+        a = 11;
+        System.out.println(b);
+
+        Person alex = new Person("Alex");
+        Person maria = alex;
+        System.out.println(alex.name + " " + maria.name);
+        alex.name = "Who?";
+        System.out.println(maria.name);
+    }
+
+    static class Person {
+        String name;
+
+        Person(String name) {
+            this.name = name;
+        }
+
+        Date date = new Date();
+        LocalDate localDate = LocalDate.now();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        String name = "Hello";
+        System.out.println(name.toUpperCase());
+
+        String string = "Hello World";
+        String result = "";
+        for (int i = 0; i < string.length(); i++) {
+            String c = Character.toString(string.charAt(i));
+            if (c.toUpperCase() == c) {
+                result += c.toLowerCase();
+            } else {
+                result += c.toUpperCase();
+            }
+        }
+
+        refTypes();
     }
 }
