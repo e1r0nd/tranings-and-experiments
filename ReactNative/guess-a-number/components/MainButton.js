10973f99b91a54922effd39cad423829f278342e
e1r0nd <a.merezhanyi@gmail.com>
2020-10-12 13:11:06 +0200

    typo

--- ReactNative/guess-a-number/components/MainButton.js
+++ ReactNative/guess-a-number/components/MainButton.js
@@ -1,17 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-import Colors from '../constants/colors';
+import Colors from "../constants/colors";
 
 const MainButton = (props) => {
   return (
-      <TouchableOpacity onPress={props.onPress}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>{props.children}</Text>
-        </View>
-      </TouchableOpacity>
-)};
-
+    <TouchableOpacity onPress={props.onPress}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
   button: {
@@ -21,10 +21,10 @@ const styles = StyleSheet.create({
     borderRadius: 25,
   },
   buttonText: {
-    color: 'white',
-    fontFamily: 'open-sans',
+    color: "white",
+    fontFamily: "open-sans",
     fontSize: 18,
-  }
+  },
 });
 
 export default MainButton;
