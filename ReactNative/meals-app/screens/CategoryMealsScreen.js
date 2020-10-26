58e7a9215d6ddecf4a9b1b95b2e3bc46c9d18226
e1r0nd <a.merezhanyi@gmail.com>
2020-10-26 11:36:33 +0100

    meals details

--- ReactNative/meals-app/screens/CategoryMealsScreen.js
+++ ReactNative/meals-app/screens/CategoryMealsScreen.js
@@ -1,34 +1,53 @@
 import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
-import { CATEGORIES } from "../data/dummy-date";
+import { View, Text, FlatList, StyleSheet } from "react-native";
 
-const CategoryMealsScreen = (props) => {
-  const categoryId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find((item) => item.id === categoryId);
+import { CATEGORIES, MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+const CategoryMealScreen = (props) => {
+  const renderMealItem = (itemData) => {
+    return (
+      <MealItem
+        title={itemData.item.title}
+        image={itemData.item.imageUrl}
+        duration={itemData.item.duration}
+        complexity={itemData.item.complexity}
+        affordability={itemData.item.affordability}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
+      />
+    );
+  };
+
+  const catId = props.navigation.getParam("categoryId");
+
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
 
   return (
     <View style={styles.screen}>
-      <Text>Category Meals Screen</Text>
-      <Text>Selected: {selectedCategory.title}</Text>
-      <Button
-        title="Go to Detail"
-        onPress={() => {
-          props.navigation.navigate("MealDetail");
-        }}
-      />
-      <Button
-        title="Back"
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item, index) => item.id}
+        renderItem={renderMealItem}
+        style={{ width: "100%" }}
       />
     </View>
   );
 };
 
-CategoryMealsScreen.navigationOptions = (navigationData) => {
-  const categoryId = navigationData.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find((item) => item.id === categoryId);
+CategoryMealScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+
   return {
     headerTitle: selectedCategory.title,
   };
@@ -39,7 +58,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    padding: 15,
   },
 });
 
-export default CategoryMealsScreen;
+export default CategoryMealScreen;
