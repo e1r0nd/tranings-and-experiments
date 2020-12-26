ee9f1ced4e3d058fafc9d4aece5180e0e0c0a5f9
e1r0nd <a.merezhanyi@gmail.com>
2020-12-26 19:17:00 +0100

    some updates

--- JavaScript/vue-01-starting-project/app.js
+++ JavaScript/vue-01-starting-project/app.js
@@ -1,9 +1,20 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Finish the course!",
+      courseGoalA: "Finish the course!",
+      courseGoalB: "Master some skills",
       vueLink: "https://vuejs.org/",
     };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      } else {
+        return this.courseGoalB;
+      }
+    },
   },
 });
 
