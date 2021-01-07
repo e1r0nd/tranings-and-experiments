fa736ecd6d19466c97288c0203ff66cca1b59b50
e1r0nd <a.merezhanyi@gmail.com>
2021-01-07 22:18:41 +0100

    one more lesson

--- JavaScript/vue-01-starting-project/app.js
+++ JavaScript/vue-01-starting-project/app.js
@@ -73,3 +73,31 @@ const eventsApp = Vue.createApp({
 });
 
 eventsApp.mount("#events");
+
+const stylingAppExample = Vue.createApp({
+  data() {
+    return {
+      boxASelected: false,
+      boxBSelected: false,
+      boxCSelected: false,
+    };
+  },
+  computed: {
+    boxBClasses() {
+      return { active: this.boxBSelected };
+    },
+  },
+  methods: {
+    boxSelected(box) {
+      if (box === "A") {
+        this.boxASelected = !this.boxASelected;
+      } else if (box === "B") {
+        this.boxBSelected = !this.boxBSelected;
+      } else if (box === "C") {
+        this.boxCSelected = !this.boxCSelected;
+      }
+    },
+  },
+});
+
+stylingAppExample.mount("#styling");
