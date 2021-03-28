f1c7f1867ec5c68adca3702a3ced4e04f49a59ea
e1r0nd <a.merezhanyi@gmail.com>
2021-03-28 17:27:52 +0200

    added acomponent

---
+++ JavaScript/vue-05-app/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <header>
+            <h1>My Friends</h1>
+        </header>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+export default {
+  components: { FriendContact },
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '123 32434',
+                    email: 'some@email.com'
+                },
+                {
+                    id: 'julie',
+                    name: 'Julie Doe',
+                    phone: '098 323234',
+                    email: 'julie@email.com'
+                }
+            ]
+        }
+    },
+    methods() {},
+    computed() {}
+};
+</script>
