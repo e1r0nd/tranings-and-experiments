9a14dcdb02bb8691c496e004c9285446543b3c6d
e1r0nd <a.merezhanyi@gmail.com>
2021-05-22 20:38:35 +0200

    card component added

--- JavaScript/React-Redux/react-complete-guide/src/App.js
+++ JavaScript/React-Redux/react-complete-guide/src/App.js
@@ -1,11 +1,27 @@
-import ExpenseItem from './components/ExpenseItem'
+import Card from "./components/UI/Card";
+import ExpenseItem from "./components/Expenses/ExpenseItem";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      expenseTitle: "Car Insurance",
+      expenseAmount: 265.65,
+      expenseDate: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      expenseTitle: "Paper",
+      expenseAmount: 23.2,
+      expenseDate: new Date(2011, 1, 8),
+    },
+  ];
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem></ExpenseItem>
-    </div>
+    <Card>
+      {expenses.map((expense) => (
+        <ExpenseItem key={expense.id} expense={expense}></ExpenseItem>
+      ))}
+    </Card>
   );
 }
 
---
+++ JavaScript/React-Redux/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+
+function ExpenseItem(props) {
+  const { expenseDate, expenseTitle, expenseAmount } = props.expense;
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={expenseDate} />
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
