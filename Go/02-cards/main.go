3d57ea05182bcd26535728bd564f029547d3cc6a
e1r0nd <a.merezhanyi@gmail.com>
2021-05-23 22:44:49 +0200

    some go exercises

--- Go/02-cards/main.go
+++ Go/02-cards/main.go
@@ -1,11 +1,14 @@
 package main
 
-func main() {
-	cards := deck{newCard(), "New One"}
-	cards = append(cards, "One more")
-	cards.print()
-}
+import "fmt"
 
-func newCard() string {
-	return "New Card"
+func main() {
+	cards := newDeck()
+	cards.saveToFile("my_cards")
+
+	cards = newDeckFromFile("my_cards")
+
+	fmt.Println("Before:", cards.toString())
+	cards.shuffle()
+	fmt.Println("After:", cards.toString())
 }
