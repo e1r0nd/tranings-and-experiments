44a9f5209caa96f6bcf0930629b62b01c1a0c7cb
e1r0nd <a.merezhanyi@gmail.com>
2021-05-25 22:07:26 +0200

    started a new lesson

---
+++ Go/03-structs/main.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contact   contactInfo
+}
+
+func main() {
+	alex := person{"Alex", "Anderson", contactInfo{"", 0}}
+	alex.print()
+	sonic := person{firstName: "Sonic", lastName: "Egghead",
+		contact: contactInfo{
+			email:   "some@mail.com",
+			zipCode: 10001,
+		}}
+	sonic.updateName("Robotnik")
+	sonic.print()
+
+	var ustas person
+	ustas.firstName = "UStas"
+	ustas.lastName = "Nemo"
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
