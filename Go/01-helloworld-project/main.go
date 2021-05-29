4ef77f3962bfa09a94679cc750cebbe2642edd3d
e1r0nd <a.merezhanyi@gmail.com>
2021-05-29 07:41:29 +0200

    some go tasks

--- Go/01-helloworld-project/main.go
+++ Go/01-helloworld-project/main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	const multiplicator = 0.234
+	var n = 123
+	fmt.Printf("Result is: %.3f \n", float32(n)*multiplicator)
+
+	car, cost := "Audi", 10
+	fmt.Printf("%v '%q is %T' %+d", car, car, car, cost)
 }
