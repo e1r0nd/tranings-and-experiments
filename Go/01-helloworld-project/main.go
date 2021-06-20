ee6e0c4f8f736268c65fd71958a6c9642d72aa51
e1r0nd <a.merezhanyi@gmail.com>
2021-06-20 22:42:31 +0100

    runes

--- Go/01-helloworld-project/main.go
+++ Go/01-helloworld-project/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode/utf8"
+)
 
 func main() {
 	fmt.Println("Hello World!")
@@ -19,4 +23,20 @@ func main() {
 	var y int8 = 127
 	y++
 	fmt.Println("2:", y)
+
+	// Rune
+	fmt.Printf("\n" + strings.Repeat("#", 20) + "\n")
+	sss := "mąż"
+	for i := 0; i < len(sss); {
+		r, size := utf8.DecodeRuneInString(sss[i:])
+		fmt.Printf("%c", r)
+		i += size
+	}
+	fmt.Printf("\n" + strings.Repeat("#", 20) + "\n")
+	for _, r := range sss {
+		fmt.Printf("%c", r)
+	}
+	fmt.Printf("\n")
+	fmt.Println("Bytes length in sss:", len(sss))
+	fmt.Println("Runes length in sss:", utf8.RuneCountInString(sss))
 }
