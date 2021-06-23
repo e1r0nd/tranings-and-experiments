66c0a9146a074cdcef2659780f7784da50d98ce5
e1r0nd <a.merezhanyi@gmail.com>
2021-06-23 21:45:36 +0100

    another step

--- Go/04-map/main.go
+++ Go/04-map/main.go
@@ -19,6 +19,29 @@ func main() {
 	fmt.Println(hexColors)
 
 	printMap(colors)
+
+	fmt.Println("---")
+	var employees map[string]string
+	fmt.Printf("Employees: %#v\n", employees)
+	fmt.Printf("No of employees: %d\n", len(employees))
+
+	people := map[int]string{1: "Dan", 2: "Rob"}
+	people[3] = "Marry"
+	fmt.Printf("People: %v\n", people)
+
+	// comma ok idiom
+	p, ok := people[4]
+	if ok {
+		fmt.Printf("Person: %v", p)
+	} else {
+		fmt.Println("No 'John' is found")
+	}
+
+	// iterator
+	for k, v := range people {
+		fmt.Printf("key: %v, value: %v;", k, v)
+	}
+	fmt.Println("\n---")
 }
 
 func printMap(c map[string]string) {
