d7a6ee3833b0c15d2a50f7d5770ff90780faf476
e1r0nd <a.merezhanyi@gmail.com>
2021-07-17 21:15:57 +0200

    hello world completed

--- Go/http-rest-api/internal/app/apiserver/apiserver.go
+++ Go/http-rest-api/internal/app/apiserver/apiserver.go
@@ -1,16 +1,24 @@
 package apiserver
 
-import "github.com/sirupsen/logrus"
+import (
+	"io"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+)
 
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
+	router *mux.Router
 }
 
 func New(config *Config) *APIServer {
 	return &APIServer{
 		config: config,
 		logger: logrus.New(),
+		router: mux.NewRouter(),
 	}
 }
 
@@ -18,8 +26,9 @@ func (s *APIServer) Start() error {
 	if err := s.configureLogger(); err != nil {
 		return err
 	}
+	s.configureRouter()
 	s.logger.Infof("Starting API server on %s", s.config.BindAddr)
-	return nil
+	return http.ListenAndServe(s.config.BindAddr, s.router)
 }
 
 func (s *APIServer) configureLogger() error {
@@ -29,4 +38,14 @@ func (s *APIServer) configureLogger() error {
 	}
 	s.logger.SetLevel(level)
 	return nil
+}
+
+func (s *APIServer) configureRouter() {
+	s.router.HandleFunc("/hello", s.handleHello()).Methods("GET")
+}
+
+func (s *APIServer) handleHello() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello, world!")
+	}
 }
